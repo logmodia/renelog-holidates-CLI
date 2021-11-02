3d6236b9f514a2361bd0347e16d68d54f7e500ce
René Modia <renelog.modia@gmail.com>
2021-11-02 14:44:04 +0100

    save

---
+++ index.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+//import fetch from 'node-fetch';
+import axios from 'axios';
+import countries from 'country-list';
+import ora from 'ora';
+import chalk from 'chalk';
+
+const spinner = ora(`Loading ${chalk.red('Loading...')}`).start();
+
+var myArgs = process.argv.slice(2); //parse arguments from the terminal by skiping node default arguments (2)
+if (myArgs.length !== 0){
+
+
+    const countryHolidates = (countryName)=>{
+    
+        const d = new Date();
+        let year = d.getFullYear();
+    
+        let countryCode = countries.getCode(`${countryName}`)
+        
+        if (countryCode != undefined){
+
+            axios.get(`https://date.nager.at/api/v3/publicholidays/${year}/${countryCode}`)
+            .then(function (response) {
+                let myArr = response.data
+                myArr.forEach(el => {
+                    console.log()
+                    console.log("------------------------------------")
+                    console.log(`${chalk.yellow("Date :")} ${el.date}\n${chalk.yellow("Name :")} ${el.name}\n${chalk.yellow("Local name :")} ${el.localName}`);
+                    
+                });
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            /*.then(function () {
+                // always executed
+            });*/
+            spinner.succeed("End of process successfull")
+            
+            console.log()
+            console.log(chalk.greenBright("Country : " + myArgs[0]))
+        
+        }else {
+            console.log()
+            console.error("You have entred a wrong country name");
+            console.log()
+        
+        }
+    
+    }
+    countryHolidates(myArgs[0])
+
+}
+else{
+    console.log("ERROR\nYou did not enter a country name")
+}
+
